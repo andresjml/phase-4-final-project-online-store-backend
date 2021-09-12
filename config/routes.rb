d0f735ccc737d6c2d3f047876471a78b2c57f393
Andres <andresjml@gmail.com>
2021-09-11 21:07:48 -0400

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :order_products
-  resources :products
-  resources :categories
-  resources :orders
-  resources :users
+  resources :order_products, only: [:index, :show, :create, :update, :destory]
+  resources :products, only: [:index, :show, :create, :update, :destory]
+  resources :categories, only: [:index, :show, :create, :update, :destory]
+  resources :orders, only: [:index, :show, :create, :update, :destory]
+  resources :users, only: [:index, :show, :create, :update, :destory]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

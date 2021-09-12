170c12dd8a879c29a0c5730fff89046ae0a9504e
Andres <andresjml@gmail.com>
2021-09-11 21:03:57 -0400

    seed file completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_many :orders, dependent: :destroy
     validates :username, presence: true, uniqueness:true
     validates :address, presence:true
-    validates :phone, presence: true, length: {maximum: 12}
+    validates :phone, presence: true, length: {maximum: 13}
     validates :first_name, presence: true
     validates :last_name, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.destroy_all
+
+
+4.times do 
+    Category.create(name:Faker::Commerce.department(max: 2, fixed_amount: true))
+end
+
+30.times do
+    Product.create(name:Faker::Commerce.product_name, description:Faker::Lorem.paragraph, price:Faker::Commerce.price, on_hand: (5..100).to_a.sample, img_url:Faker::Company.logo, category_id:(1..4).to_a.sample)
+end
+
+5.times do
+    User.create(username:Faker::Internet.username, address: Faker::Address.full_address, phone:Faker::PhoneNumber.cell_phone, first_name:Faker::Name.first_name, last_name:Faker::Name.last_name)
+end
+
+Order.create(user_id:1, paid?:false)
+Order.create(user_id:2, paid?:false)
+Order.create(user_id:3, paid?:false)
+
+OrderProduct.create(order_id:1, product_id: 1, product_qty:(1..5).to_a.sample)
+OrderProduct.create(order_id:1, product_id: 3, product_qty:(1..5).to_a.sample)
+OrderProduct.create(order_id:1, product_id: 5, product_qty:(1..5).to_a.sample)
+OrderProduct.create(order_id:1, product_id: 7, product_qty:(1..5).to_a.sample)
+OrderProduct.create(order_id:1, product_id: 9, product_qty:(1..5).to_a.sample)
+OrderProduct.create(order_id:2, product_id: 2, product_qty:(1..5).to_a.sample)
+OrderProduct.create(order_id:2, product_id: 4, product_qty:(1..5).to_a.sample)
+OrderProduct.create(order_id:2, product_id: 6, product_qty:(1..5).to_a.sample)
+OrderProduct.create(order_id:2, product_id: 8, product_qty:(1..5).to_a.sample)
+OrderProduct.create(order_id:2, product_id: 10, product_qty:(1..5).to_a.sample)
+OrderProduct.create(order_id:2, product_id: 12, product_qty:(1..5).to_a.sample)
+OrderProduct.create(order_id:2, product_id: 14, product_qty:(1..5).to_a.sample)
+OrderProduct.create(order_id:2, product_id: 16, product_qty:(1..5).to_a.sample)
+OrderProduct.create(order_id:2, product_id: 18, product_qty:(1..5).to_a.sample)
+OrderProduct.create(order_id:2, product_id: 20, product_qty:(1..5).to_a.sample)
+OrderProduct.create(order_id:3, product_id: 30, product_qty:(1..5).to_a.sample)

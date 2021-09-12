2d6281ed1e8d95d5c6968751928c56d79ade0573
Andres <andresjml@gmail.com>
2021-09-11 21:08:59 -0400

    fixing typo in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :order_products, only: [:index, :show, :create, :update, :destory]
-  resources :products, only: [:index, :show, :create, :update, :destory]
-  resources :categories, only: [:index, :show, :create, :update, :destory]
-  resources :orders, only: [:index, :show, :create, :update, :destory]
-  resources :users, only: [:index, :show, :create, :update, :destory]
+  resources :order_products, only: [:index, :show, :create, :update, :destroy]
+  resources :products, only: [:index, :show, :create, :update, :destroy]
+  resources :categories, only: [:index, :show, :create, :update, :destroy]
+  resources :orders, only: [:index, :show, :create, :update, :destroy]
+  resources :users, only: [:index, :show, :create, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

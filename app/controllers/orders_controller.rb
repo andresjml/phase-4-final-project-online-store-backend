346c3dd0f845e8baf7ae8d29341beae6113cb8ec
Andres <andresjml@gmail.com>
2021-09-11 22:36:28 -0400

    added index, show, create, update, destroy methods to OrdersController

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,45 @@
 class OrdersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+   
+    def index
+        orders=Order.all 
+        render json: orders
+    end
+
+    def show
+        order= find_order
+        render json:order
+    end
+
+    def create
+        order=Order.create(order_params)
+        render json:order
+    end
+
+    def update
+        order= find_order
+        order.update(order_params)
+        render json:order
+    end
+
+    def destroy
+        order= Order.find(params[:id])
+        order.destroy
+        head :no_content
+    end
+
+    private
+
+    def find_order
+        Order.find(params[:id])
+    end
+
+    def order_params
+        params.permit(:user_id, :paid?)
+    end
+
+    def render_not_found_response
+        render json: { error: "Order not found" }, status: :not_found
+    end
 end
--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,4 +1,4 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id, :paid?
+  attributes :id, :user_id, :paid?
   has_one :user
 end

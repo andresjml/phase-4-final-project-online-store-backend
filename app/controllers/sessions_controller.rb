57dce8c48818d6a3bb719a0a6e8374e6d7a2cf7a
Andres <andresjml@gmail.com>
2021-09-15 08:19:33 -0400

    added products to categories serializer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController<ApplicationController
     def create
         user = User.find_by(username: params[:username])
         session[:user_id] = user.id
-       
+     
         render json: user
     end
 
